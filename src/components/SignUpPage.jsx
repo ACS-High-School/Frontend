e8a0fc75cf92042501f5b61b9b3523a8d9a7629e
sw801733 <sw801733@naver.com>
2024-01-30 01:33:25 +0900

    style : 회원가입 폼 스타일  추가
    - 기본적인 스타일 폼 추가

--- src/components/SignUpPage.jsx
+++ src/components/SignUpPage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import "../styles/SignUpPage.css"; // 스타일링을 위한 CSS 파일을 임포트합니다.
 
 const SignForm = (props) => {
   const {
@@ -19,87 +20,91 @@ const SignForm = (props) => {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div>
-        <label htmlFor="email">이메일</label>
-        <input
-          id="email"
-          type="text"
-          placeholder="test@email.com"
-          {...register("email", {
-            required: "이메일은 필수 입력입니다.",
-            pattern: {
-              value:
-                /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i,
-              message: "이메일 형식에 맞지 않습니다.",
-            },
-          })}
-        />
-        {errors.email && <small role="alert">{errors.email.message}</small>}
-      </div>
-      <div>
-        <label htmlFor="nickname">닉네임</label>
-        <input
-          id="nickname"
-          type="text"
-          placeholder="닉네임을 입력하세요"
-          {...register("nickname", {
-            required: "닉네임은 필수 입력입니다.",
-            minLength: {
-              value: 3,
-              message: "닉네임은 최소 3자 이상이어야 합니다.",
-            },
-          })}
-        />
-        {errors.nickname && (
-          <small role="alert">{errors.nickname.message}</small>
-        )}
-      </div>
-      <div className="form-control__items">
-        <label htmlFor="password">비밀번호</label>
-        <input
-          id="password"
-          type="password"
-          placeholder="*******"
-          {...register("password", {
-            required: "비밀번호는 필수 입력입니다.",
-            minLength: {
-              value: 7,
-              message: "7자리 이상 비밀번호를 입력하세요.",
-            },
-          })}
-        />
-        {errors.password && (
-          <small role="alert">{errors.password.message}</small>
-        )}
-      </div>
-      <div>
-        <label htmlFor="passwordConfirm">비밀번호 확인</label>
-        <input
-          id="passwordConfirm"
-          type="password"
-          placeholder="*******"
-          {...register("passwordConfirm", {
-            required: "비밀번호 확인은 필수 입력입니다.",
-            minLength: {
-              value: 7,
-              message: "7자리 이상 비밀번호를 사용하세요.",
-            },
-            validate: {
-              check: (val) => {
-                if (getValues("password") !== val) {
-                  return "비밀번호가 일치하지 않습니다.";
-                }
+    <div className="sign-form-container">
+      <form onSubmit={handleSubmit(onSubmit)} className="sign-form">
+        <div>
+          <label htmlFor="email">이메일</label>
+          <input
+            id="email"
+            type="text"
+            placeholder="test@email.com"
+            {...register("email", {
+              required: "이메일은 필수 입력입니다.",
+              pattern: {
+                value:
+                  /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i,
+                message: "이메일 형식에 맞지 않습니다.",
               },
-            },
-          })}
-        />
-        {errors.passwordConfirm && (
-          <small role="alert">{errors.passwordConfirm.message}</small>
-        )}
-      </div>
-      <button type="submit">회원가입 완료</button>
-    </form>
+            })}
+          />
+          {errors.email && (
+            <small role="alert">{errors.email.message}</small>
+          )}
+        </div>
+        <div>
+          <label htmlFor="nickname">닉네임</label>
+          <input
+            id="nickname"
+            type="text"
+            placeholder="닉네임을 입력하세요"
+            {...register("nickname", {
+              required: "닉네임은 필수 입력입니다.",
+              minLength: {
+                value: 3,
+                message: "닉네임은 최소 3자 이상이어야 합니다.",
+              },
+            })}
+          />
+          {errors.nickname && (
+            <small role="alert">{errors.nickname.message}</small>
+          )}
+        </div>
+        <div className="form-control__items">
+          <label htmlFor="password">비밀번호</label>
+          <input
+            id="password"
+            type="password"
+            placeholder="*******"
+            {...register("password", {
+              required: "비밀번호는 필수 입력입니다.",
+              minLength: {
+                value: 7,
+                message: "7자리 이상 비밀번호를 입력하세요.",
+              },
+            })}
+          />
+          {errors.password && (
+            <small role="alert">{errors.password.message}</small>
+          )}
+        </div>
+        <div>
+          <label htmlFor="passwordConfirm">비밀번호 확인</label>
+          <input
+            id="passwordConfirm"
+            type="password"
+            placeholder="*******"
+            {...register("passwordConfirm", {
+              required: "비밀번호 확인은 필수 입력입니다.",
+              minLength: {
+                value: 7,
+                message: "7자리 이상 비밀번호를 사용하세요.",
+              },
+              validate: {
+                check: (val) => {
+                  if (getValues("password") !== val) {
+                    return "비밀번호가 일치하지 않습니다.";
+                  }
+                },
+              },
+            })}
+          />
+          {errors.passwordConfirm && (
+            <small role="alert">{errors.passwordConfirm.message}</small>
+          )}
+        </div>
+        <button type="submit">회원가입 완료</button>
+      </form>
+    </div>
   );
 };
 
