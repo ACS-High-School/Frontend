40d7821f016e5b88959682875e7b9005e54352a3
sw801733 <sw801733@naver.com>
2024-02-17 20:19:17 +0900

    fix : http-Proxy 엔드포인트 중복 에러 수정
    localhost:3000/select 에 대한 중복 요청 수정
    - select 페이지로 navigate
    - 8080 포트로 select 요청 수행 test 로 변경

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function(app) {
   app.use(
-    createProxyMiddleware("/select", {
+    createProxyMiddleware("/test", {
       target: "http://localhost:8080",
       changeOrigin: true,
     })
