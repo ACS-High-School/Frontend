244681ad422273afc6ec9bf0ca1b52145dbe7113
sw801733 <sw801733@naver.com>
2024-02-19 23:57:53 +0900

    feat : 인가가 필요한 사이트에 토큰 검증 추가
    - 로그인 페이지와 회원가입에는 인가가 필요하지 않으므로 "/" 와 "/signup" 은 제외
    - 나머지 "/select", "/inference", "fl" 에는 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import LoginPage from './components/LoginPage';
-import SignUpPage from './components/SignUpPage';
-import MyPage from './components/MyPage';
-import MainSelectionPage from './components/MainSelectionPage';
-import InferencePage from './components/InferencePage';
-import FLPage from './components/FLPage';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import LoginPage from "./components/LoginPage";
+import SignUpPage from "./components/SignUpPage";
+import MyPage from "./components/MyPage";
+import MainSelectionPage from "./components/MainSelectionPage";
+import InferencePage from "./components/InferencePage";
+import FLPage from "./components/FLPage";
+import { verifyToken } from "./api/api"; // verifyToken 함수 임포트
+
+const ProtectedRoute = ({ children }) => {
+  const isLoggedIn = verifyToken(); // 라우트 접근 시에만 verifyToken 호출
+  return isLoggedIn ? children : <Navigate to="/" replace />;
+};
 
 function App() {
   return (
@@ -13,10 +24,38 @@ function App() {
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/mypage" element={<MyPage />} />
-        <Route path="/select" element={<MainSelectionPage />} />
-        <Route path="/inference" element={<InferencePage />} />
-        <Route path="/fl" element={<FLPage />} />
+        <Route
+          path="/mypage"
+          element={
+            <ProtectedRoute>
+              <MyPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/select"
+          element={
+            <ProtectedRoute>
+              <MainSelectionPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/inference"
+          element={
+            <ProtectedRoute>
+              <InferencePage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/fl"
+          element={
+            <ProtectedRoute>
+              <FLPage />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </Router>
   );
