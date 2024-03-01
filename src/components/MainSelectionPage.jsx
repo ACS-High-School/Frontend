b113fb73b8588b6408b4f82f92b52b3a8d53ddeb
sw801733 <sw801733@naver.com>
2024-03-01 22:57:15 +0900

    fix : 그룹 생성 시 코드 보이는 기능 제거

--- src/components/MainSelectionPage.jsx
+++ src/components/MainSelectionPage.jsx
@@ -6,13 +6,10 @@ import { useNavigate } from 'react-router-dom';
 const SelectionPage = () => {
     const navigate = useNavigate(); // useNavigate 훅 사용
     const [hoverFL, setHoverFL] = useState(false);
-    const [showGroupCode, setShowGroupCode] = useState(false); // 그룹 코드 표시 상태
     const [showJoinForm, setShowJoinForm] = useState(false); // 참여 폼 표시 상태
     const [groupCode, setGroupCode] = useState(''); // 그룹 코드 상태 추가
 
     const handleCreateGroup = () => {
-        setShowGroupCode(true); // 그룹 코드 표시
-        setShowJoinForm(false); // 참여 폼 숨김
         const randomCode = Math.random().toString(36).substring(2, 8);
         setGroupCode(randomCode); // 랜덤 그룹 코드 설정
         navigate(`/fl/${randomCode}`);
@@ -20,7 +17,6 @@ const SelectionPage = () => {
 
     const handleJoinGroup = () => {
         setShowJoinForm(true); // 참여 폼 표시
-        setShowGroupCode(false); // 그룹 코드 숨김
     };
 
     return (
@@ -36,7 +32,6 @@ const SelectionPage = () => {
                         <>
                             <button className="fl-button" onClick={handleCreateGroup}>그룹 생성</button>
                             <button className="fl-button" onClick={handleJoinGroup}>그룹 참여</button>
-                            {showGroupCode && <div className="group-code">그룹 코드: {groupCode}</div>}
                             {showJoinForm && (
                                 <form className="join-form">
                                     <input type="text" placeholder="그룹 코드 입력" />
