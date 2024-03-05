3025f0e7d5f2a1f5b63304dd31020ceb6ed38f5a
sw801733 <sw801733@naver.com>
2024-03-06 01:12:56 +0900

    AHSK-227 fix : 로그인 페이지 /login 으로 경로 수정
    - 루트 페이지에서 /login 으로 변경

--- src/App.js
+++ src/App.js
@@ -15,7 +15,6 @@ import { verifyToken } from "./api/api"; // verifyToken 함수 임포트
 import Layout from "./layout/Layout";
 import { Container } from "react-bootstrap";
 
-
 const ProtectedRoute = ({ children }) => {
   const isLoggedIn = verifyToken(); // 라우트 접근 시에만 verifyToken 호출
   return isLoggedIn ? children : <Navigate to="/" replace />;
@@ -25,9 +24,9 @@ function App() {
   return (
     <Router>
       <Layout>
-        <Container style={{ minHeight: '75vh' }}>
+        <Container style={{ minHeight: "75vh" }}>
           <Routes>
-            <Route path="/" element={<LoginPage />} />
+            <Route path="/login" element={<LoginPage />} />
             <Route path="/signup" element={<SignUpPage />} />
             <Route
               path="/mypage"
