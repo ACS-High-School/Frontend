58481a9f142729c7bbf6c74e59b1f9a00a28ad99
sw801733 <sw801733@naver.com>
2024-03-06 01:51:30 +0900

    AHSK-228 fix : 회원가입 페이지의 헤더 변경
    - /signup 인 경우 B3O 와 로그인 버튼만 보이도록 설정

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -19,34 +19,39 @@ const Header = () => {
   const isMyPage = location.pathname === "/mypage";
   const isHome = location.pathname === "/";
   const isLogin = location.pathname === "/login"; // 로그인 페이지인지 확인하는 조건
+  const isSignUp = location.pathname === "/signup"; // 회원가입 페이지인지 확인하는 조건
 
   return (
     <header>
       <Navbar bg="light" expand="lg">
         <Container>
-          {isLogin ? (
-            // 로그인 페이지인 경우, B3O 버튼만 표시
-            <Navbar.Brand href="/">B3O</Navbar.Brand>
-          ) : (
-            // 그 외의 경우, 전체 Navbar를 표시
+          <Navbar.Brand href="/">B3O</Navbar.Brand>
+          {!isLogin && (
+            // 로그인 페이지가 아닌 경우에만 Navbar.Toggle 및 Navbar.Collapse 표시
             <>
-              <Navbar.Brand href="/">B3O</Navbar.Brand>
               <Navbar.Toggle aria-controls="basic-navbar-nav" />
               <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ms-auto">
-                  {!isHome && <Nav.Link href="/select">Main</Nav.Link>}
+                  {!isHome && !isSignUp && (
+                    <Nav.Link href="/select">Main</Nav.Link>
+                  )}
                   {isMyPage ? (
                     <Button variant="outline-danger" onClick={handleSignOut}>
                       Logout
                     </Button>
-                  ) : isHome ? (
-                    <Button
-                      variant="outline-primary"
-                      onClick={() => navigate("/login")}
-                    >
-                      Login
-                    </Button>
                   ) : (
+                    (isHome || isSignUp) && (
+                      // 홈 또는 회원가입 페이지인 경우 로그인 버튼 표시
+                      <Button
+                        variant="outline-primary"
+                        onClick={() => navigate("/login")}
+                      >
+                        Login
+                      </Button>
+                    )
+                  )}
+                  {!isHome && !isSignUp && !isMyPage && (
+                    // 홈, 회원가입, 마이페이지가 아닌 경우 프로필 링크 표시
                     <Nav.Link href="/mypage">Profile</Nav.Link>
                   )}
                 </Nav>
