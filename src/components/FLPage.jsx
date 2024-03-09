246c89d0a488b6de072bb7915690d70b04ba4c2d
sw801733 <sw801733@naver.com>
2024-03-09 15:24:26 +0900

    AHSK-256 fix : 유저 정보 표시에 로딩 중 추가

--- src/components/FLPage.jsx
+++ src/components/FLPage.jsx
@@ -11,25 +11,30 @@ function FLPage() {
     { id: 4, name: 'User4', fileUploaded: false }
   ]);
 
+  const [isLoading, setIsLoading] = useState(true); // 로딩 상태 추가
+
   const { groupCode } = useParams(); // useParams를 사용하여 URL에서 groupCode를 가져옵니다.
 
   useEffect(() => {
+    setIsLoading(true); // API 요청 시작 시 로딩 상태를 true로 설정
     api.post('/group/users', { groupCode })
       .then(response => {
-        // 서버 응답에서 각 User 객체의 username 추출
         const updatedUsers = users.map((user, index) => {
-          // user1, user2, user3, user4에 해당하는 서버 응답 데이터에서 username 추출
-          const serverUser = response.data[`user${index + 1}`]; // index는 0부터 시작하므로 +1
+          const serverUser = response.data[`user${index + 1}`];
           return serverUser
-            ? { ...user, username: serverUser.username } // 서버 응답에 해당 사용자 정보가 있으면 username 추가 또는 업데이트
-            : { ...user, username: '' }; // 서버 응답에 사용자 정보가 없으면 username을 빈 문자열로 설정
+            ? { ...user, username: serverUser.username }
+            : { ...user, username: '아직 입장 안함' }; // 정보가 없으면 "아직 입장 안함"으로 설정
         });
-  
-        setUsers(updatedUsers); // 업데이트된 사용자 목록으로 상태 업데이트
+
+        setUsers(updatedUsers);
+        setIsLoading(false); // API 요청 완료 후 로딩 상태를 false로 설정
       })
-      .catch(error => console.error("There was an error!", error));
-  }, [groupCode]); // 의존성 배열에는 groupCode만 포함
-  
+      .catch(error => {
+        console.error("There was an error!", error);
+        setIsLoading(false); // 에러 발생 시 로딩 상태를 false로 설정
+      });
+  }, [groupCode]);
+
   
   
   
@@ -52,18 +57,19 @@ function FLPage() {
         <button onClick={reloadPage} className="reload-btn">Reload</button>
       </div>  
       <div className="user-list">
-        {users.map(user => (
-          <div key={user.id} className={`user-component ${user.fileUploaded ? 'uploaded' : ''}`}>
-            <span>{user.name}</span>
-            <span>
-              {user.username ? user.username : "아직 입장 안함"} {/* username이 있으면 표시하고, 없으면 "정보 없음" 표시 */}
-            </span>
-          </div>
-        ))}
+        {isLoading ? (
+          <div>Loading...</div> // 로딩 중이면 로딩 인디케이터 표시
+        ) : (
+          users.map(user => (
+            <div key={user.id} className={`user-component ${user.fileUploaded ? 'uploaded' : ''}`}>
+              <span>{user.name}</span>
+              <span>{user.username}</span> {/* "아직 입장 안함" 메시지는 API 응답 처리 시 추가됨 */}
+            </div>
+          ))
+        )}
       </div>
     </div>
   );
-  
 }
 
 export default FLPage;
