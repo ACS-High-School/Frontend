1a9ac0114654bc4b34bc1386311677e8f82df0b2
sw801733 <sw801733@naver.com>
2024-03-14 23:34:56 +0900

    Revert "AHSK-277 fix : 그룹 생성 시 pop 안뜨는 버그 수정"
    This reverts commit 538dff06110bc0234db0a951b65d4480744e00e5.

--- src/components/MainSelectionPage.jsx
+++ src/components/MainSelectionPage.jsx
@@ -20,11 +20,9 @@ const SelectionPage = () => {
         setShowJoinForm(false); // 참여 폼은 숨김
     };
 
-    const handleCreateGroup = async (event) => {
-        event.preventDefault();
+    const handleCreateGroup = async () => {
         const userConfirmed = window.confirm("그룹을 생성합니다"); // 사용자에게 확인 요청
         if (userConfirmed) {
-
             const randomCode = Math.floor(Math.random() * (10**6 - 10**5) + 10**5);
             setGroupCode(randomCode);
 
