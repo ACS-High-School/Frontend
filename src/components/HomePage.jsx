004750f9e28e902378a8d0567efba1469b305a5d
sw801733 <sw801733@naver.com>
2024-03-20 22:12:20 +0900

    AHSK-301 style : HomePage 사이트 소개 gif 추가

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../styles/HomePage.css'; // HomePage에 맞는 스타일시트 경로
-import homeImage from '../assets/home.png'; // home.png 이미지를 assets 폴더에서 불러옵니다.
+import homeImage from '../assets/home.gif'; // home.png 이미지를 assets 폴더에서 불러옵니다.
 
 const HomePage = () => {
     return (
