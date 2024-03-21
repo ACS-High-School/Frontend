8be23a20c91bdf26bdca6ae2a028af70cdd83f0b
sw801733 <sw801733@naver.com>
2024-03-21 14:48:13 +0900

    AHSK-301 style : HomePage 영역 구분 및 버튼 위치 조정

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import '../styles/HomePage.css'; // HomePage에 맞는 스타일시트 경로
-import homeImage from '../assets/home.gif'; // home.png 이미지를 assets 폴더에서 불러옵니다.
+import '../styles/HomePage.css'; // CSS 파일 경로를 확인해주세요.
+import homeImage from '../assets/home.gif'; // GIF 이미지 경로를 지정해주세요.
+import Button from 'react-bootstrap/Button';
 
 const HomePage = () => {
     return (
         <div className="home-container">
-            <div className="home-section">
-                <img src={homeImage} alt="Home" className="home-image"/>
+            <div className="home-image-container">
+                {/* <img src={homeImage} alt="Home" className="home-image"/> */}
+            </div>
+            <div className="home-auth-container">
+                <Button className="auth-button" variant="primary">Enter</Button>
             </div>
         </div>
     );
